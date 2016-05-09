06e87b2e525eeee24fff1c5c85cca2a262e4defe
Murphy Randle <murphyrandle@gmail.com>
2016-05-09 07:30:11 -0600

    Finish

--- Main.elm
+++ Main.elm
@@ -1,12 +1,41 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
+import Html.App
 
 -- Main
 
-main = div [] [text "Hello!"]
+main : Program Never
+main = Html.App.beginnerProgram
+  { model = 0
+  , update = update
+  , view = view
+  }
 
 -- Model
+type alias Model = Int
+
 -- Messages
+
+type Msg
+  = Inc
+  | Dec
+
 -- Update
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Inc -> model + 1
+    Dec -> model - 1
+
 -- View
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] [text "Count: ", text <| toString model]
+    , button [onClick Dec] [text "Decrement"]
+    , button [onClick Inc] [text "Increment"]
+    ]
